48f8cf8ddc15fea075f0870917fda813c020574d
BojanMichael <geh-weg@paranoya.ch>
2012-06-14 11:11:15 +0200

    Gui

--- src/gui/ConverterFrame.java
+++ src/gui/ConverterFrame.java
@@ -1,13 +1,54 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+
+import javax.swing.JComboBox;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ConverterFrame extends JFrame 
 {
 	public ConverterFrame()
 	{
-		setTitle("Converter");
-		setSize(400,400);
-		setVisible(true);
+		this.setLayout(new BorderLayout());
+		JPanel layoutPanel = new JPanel();
+		layoutPanel.setLayout(new FlowLayout());
+		
+		JComboBox<String> firstUnit = new JComboBox<String>();
+		firstUnit.addItem("Metttter");
+		firstUnit.addItem("MiliMettter");
+		firstUnit.addItem("Ellenlänge");
+		firstUnit.setEditable(true);
+//		firstUnit.addActionListener(listener);
+		
+
+		
+		JComboBox<String> secondUnit = new JComboBox<String>();
+		secondUnit.addItem("Metttter");
+		secondUnit.addItem("MiliMettter");
+		secondUnit.addItem("Ellenlänge");
+		secondUnit.setEditable(true);
+//		secondUnit.addActionListener(listener);
+		
+		JTextField inputField = new JTextField();
+		
+		JTextField outputField = new JTextField();
+
+		layoutPanel.add(firstUnit);
+		layoutPanel.add(inputField);
+		layoutPanel.add(secondUnit);
+		layoutPanel.add(outputField);
+		
+		Insests i = new Insets(10,10,10,10);
+
+		this.add(new JSeparator(), BorderLayout.NORTH);
+		this.add(layoutPanel,BorderLayout.CENTER);
+		
+		
 	}
 }
--- src/gui/Launcher.java
+++ src/gui/Launcher.java
@@ -1,9 +1,17 @@
 package gui;
 
+import javax.swing.JFrame;
+
 public class Launcher 
 {
+	private static final int WIDTH = 500, HEIGHT = 500;
+	
 	public static void main(String[] args) 
 	{
-		
+		ConverterFrame cF = new ConverterFrame();
+		cF.setSize(WIDTH, HEIGHT);
+		cF.setTitle("Converter");
+		cF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cF.setVisible(true);
 	}
 }

b073551031237b63671c6a37ade30f49eaec39ab
BojanMichael <geh-weg@paranoya.ch>
2012-06-14 10:33:32 +0200

    Frist gui tests

---
+++ src/gui/ConverterFrame.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class ConverterFrame extends JFrame 
+{
+	public ConverterFrame()
+	{
+		setTitle("Converter");
+		setSize(400,400);
+		setVisible(true);
+	}
+}
---
+++ src/gui/Launcher.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class Launcher 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}

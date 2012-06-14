c7e18523ad06a0ff8492c6ebd1ab79850fe17411
bj <B@asdf>
2012-06-14 08:46:55 +0200

    aödsjflaj

--- src/xmlAdapter/MapAdapterAsArray.java
+++ src/xmlAdapter/MapAdapterAsArray.java
@@ -1,6 +1,26 @@
 package xmlAdapter;
 
-public class MapAdapterAsArray //extends XmlAdapter<LanguageHandler[], Map<>>
+import java.util.Map;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import unit.MyUnit;
+import language.LanguageHandler;
+
+public class MapAdapterAsArray extends XmlAdapter<LanguageHandler[], Map<String, LanguageHandler>>
 {
-//	public LanguageHandler[] marshal(Map<> map) 
+
+	@Override
+	public LanguageHandler[] marshal(Map<String, LanguageHandler> arg0)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, LanguageHandler> unmarshal(LanguageHandler[] arg0)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }

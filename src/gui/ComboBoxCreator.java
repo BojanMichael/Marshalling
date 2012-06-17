de734ee85521fb35a1e1170320c12a851ca11332
bj <B@asdf>
2012-06-17 17:33:03 +0200

    asdf(bj)

--- src/gui/ComboBoxCreator.java
+++ src/gui/ComboBoxCreator.java
@@ -1,26 +1,71 @@
 package gui;
 
-import javax.swing.JComboBox;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import javax.swing.JComboBox;
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBException;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import org.xml.sax.SAXException;
 import unit.MyUnit;
 import unit.MyUnits;
 
 @SuppressWarnings("serial")
 public class ComboBoxCreator extends JComboBox<String> //should be an object with a string and a key --> string: Kilometer and the key: km for searching the correct reference
 {
-	public ComboBoxCreator(MyUnits unitsList)
+	public ComboBoxCreator()
 	{
-		createComoBox(unitsList);
+		createComoBox();
 	}
 	
-	private void createComoBox(MyUnits unitsList)
+	private void createComoBox()
 	{		
-		for(MyUnit unit : unitsList.getUnitList())
+		for(MyUnit unit : getXmlUnitList().getUnitList())
 			addItem(unit.getUnitLabel());
 		
 //		addItem("Metttter");
 //		addItem("MiliMettter");
 //		addItem("Ellenlänge");
 		setEnabled(true);
+	}
+	
+	private MyUnits getXmlUnitList()
+	{
+		MyUnits lengthUnits = null;
+		try 
+		{
+			SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = sf.newSchema(new File(Launcher.SCHEMA_LOC+Launcher.SCHEMA1_NAME));
+			lengthUnits = (MyUnits)Factory.loadInstance(new FileInputStream(Launcher.OBJECTS_LOC+Launcher.OBJECT_NAME), schema, MyUnits.class);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("! File not found ! "+e.getMessage());
+		} 
+		catch (JAXBException e) 
+		{
+			System.out.println(e.getCause());
+			System.out.println("! JAXB Exception ! "+e.getMessage());
+		} 
+		catch (SAXException e) 
+		{
+			System.out.println("! SAX Exception ! "+e.getMessage());
+		}
+
+		
+		return lengthUnits;
+		
+//		if(lengthUnits != null)
+//		{
+//			for(MyUnit el : lengthUnits.getUnitList())
+//			{
+//				System.out.println("--------------------------------------------------------------------------");
+//				System.out.println(Launcher.DEF_LANG+": ["+el.getUnitLabel()+"] "+el.getUnitName(Launcher.DEF_LANG)+" - "+el.getDescription(Launcher.DEF_LANG));
+//				System.out.println("--------------------------------------------------------------------------");
+//			}
+//		}
 	}
 }
